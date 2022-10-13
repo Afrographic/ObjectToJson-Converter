55dda26e4dc9cde8ecef9065734b08a245083a71
unknown <bruneltesse@gmail.com>
2022-10-13 11:19:49 +0100

    Moving

---
+++ app.js
@@ -0,0 +1,121 @@
+
+
+// Hosted on Netlify
+async function dummyLoader() {
+    $(".spinnerObject").classList.add("spinner");
+    await sleep(500);
+    $(".spinnerObject").classList.remove("spinner");
+}
+
+async function generateDummyData() {
+    $("#renderArea").innerHTML = "";
+    await dummyLoader();
+    let userInput = $("#schema").value;
+    if (validDartClassSchema(userInput)) {
+
+        let fieldString = extractFields(userInput);
+        let fieldObjects = parseFieldsStringToFieldObject(fieldString);
+        let dummyData = renderDummyDataWithParameters(fieldObjects, userInput);
+
+        $("#renderArea").classList.remove("empty");
+        $("#renderArea").innerHTML = `${dummyData}`;
+        // Show successfull work
+        showInfoWidget($(".sucessMessage"))
+        // Show the copy button
+        showCopyButton();
+        // Mobile behavior
+        showRenderViewForMobile();
+        hideGenerateButton();
+    } else {
+        $("#renderArea").classList.add("empty");
+        showInfoWidget($(".errorBox"));
+        hideCopyButton();
+    }
+}
+
+function showRenderViewForMobile() {
+    hideInputViewForMobile();
+    $(".renderBoxMobile").classList.remove("disabled");
+    $(".renderBoxMobile").classList.add("active");
+}
+
+function hideInputViewForMobile() {
+    $(".inputBoxMobile").classList.add("disabled");
+    $(".inputBoxMobile").classList.remove("active");
+}
+
+function showInputViewForMobile() {
+    HideRenderViewForMobile();
+    $(".inputBoxMobile").classList.remove("disabled");
+    $(".inputBoxMobile").classList.add("active");
+}
+
+function HideRenderViewForMobile() {
+    $(".renderBoxMobile").classList.add("disabled");
+    $(".renderBoxMobile").classList.remove("active");
+}
+
+function hideGenerateButton() {
+    $(".renderButton").classList.add("disabled");
+}
+function showGenerateButton() {
+    $(".renderButton").classList.remove("disabled");
+}
+
+
+function showCopyButton() {
+    $(".copyWrapper").classList.remove("disabled");
+    $(".copyWrapper").classList.add("active");
+}
+
+function hideCopyButton() {
+    $(".copyWrapper").classList.add("disabled");
+    $(".copyWrapper").classList.remove("active");
+}
+
+async function showInfoWidget(infoWidget) {
+    infoWidget.classList.remove("disabled");
+    infoWidget.classList.add("visible");
+    await sleep(2000);
+    infoWidget.classList.remove("visible");
+    infoWidget.classList.add("disabled");
+}
+
+function showToolTip() {
+    $(".renderButton").addEventListener("mouseover", function (e) {
+        $(".toolTip").classList.remove("hide");
+        $(".toolTip").classList.add("show");
+    })
+    $(".renderButton").addEventListener("mouseout", function (e) {
+        $(".toolTip").classList.add("hide");
+        $(".toolTip").classList.remove("show");
+    })
+}
+
+async function copy() {
+    await navigator.clipboard.writeText($("#renderArea").innerText.trim());
+    showInfoWidget($(".successCopy"));
+}
+
+
+showToolTip();
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function resetFields() {
+    $("#schema").value = '';
+    $("#renderArea").innerHTML = '';
+    $("#renderArea").classList.add("empty");
+    hideCopyButton();
+    showInputViewForMobile();
+    showGenerateButton();
+}
+
+// class Afro { int idUser; double?height; bool isFound; String name; }
+
+// https://object-generator.trader-app.net/
+// add add to homescreen feature
+
+
---
+++ generator.js
@@ -0,0 +1,169 @@
+// ===========================
+/*
+
+DART DUMMY DATA GENERATOR
+@author Afrographix - Pharaon Level - ave Ankh  - Hotep
+
+*/
+
+// ==========================
+
+// Class Schema
+/*
+InPut
+
+class FlashCardQuestion {
+  int idQuestion;
+  double question;
+  bool isOnline;
+  String answer;
+}
+
+Ouput
+
+new FlashCardQuestion(
+    1,1.5,false,"trueShit"
+)
+
+or
+FlashCardQuestion {
+  idQuestion:1;
+  question:1.5;
+  isOnline:false;
+  String:"trushit";
+}
+
+*/
+class Field {
+    constructor(type, label) {
+        this.type = type;
+        this.label = label;
+    }
+}
+
+function init() {
+
+}
+
+function randomIntGenerator(max) {
+    return Math.floor(Math.random() * max);
+}
+function randomDoubleGenerator() {
+    return (Math.random() * 1000).toFixed(2);
+}
+
+function randomBoolGenerator() {
+    let date = new Date();
+    let ts = date.valueOf();
+    return ts % 2 == 0;
+}
+
+function randomStringGenerator() {
+    let alphabet = ['Steve', 'b', 'c', 'Ankh', 'Ankh', 'Afro', 'A', 'Ankh', 'C', 'D', 'E', 'F', 'G', 'Afro', 'I', 'Ankh', 'K', 'L', 'Afro', 'N', 'O', 'P', 'Q', 'Afro', 'e', 'f', 'Afro', 'h', 'i', 'j', 'k', 'l', 'm', 'Afro', 'o', 'Afro', 'q', 'r', 's', 't', 'Ankh', 'v', 'w', 'x', 'Afro', 'S', 'Soft', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    let string = "";
+    for (let i = 0; i <= 8; i++) {
+        string += alphabet[randomIntGenerator(alphabet.length - 1)];
+    }
+
+    return `"${string}"`;
+}
+
+function validDartClassSchema(dartClassSchema) {
+    dartClassSchema = dartClassSchema.trim();
+    return /^(class|Class)\s+[a-zA-Z_]+\s*{\s+((int|double|bool|String)\??\s+[a-zA-Z_0-9]+;\s*)+\s*}\s*/.test(dartClassSchema);
+}
+
+function generateRandom(dataType) {
+    switch (dataType) {
+        case 'int':
+            return randomIntGenerator(1000);
+        case 'int?':
+            return randomIntGenerator(1000);
+        case 'double':
+            return randomDoubleGenerator();
+        case 'double?':
+            return randomDoubleGenerator();
+        case 'bool':
+            return randomBoolGenerator()
+        case 'bool?':
+            return randomBoolGenerator()
+        case 'String':
+            return randomStringGenerator();
+        case 'String?':
+            return randomStringGenerator();
+        default:
+            return -1;
+    }
+}
+
+function extractClassName(dartClassSchema) {
+    let re = /(class|Class)\s+([a-zA-Z_]+).*/;
+    let m = re.exec(dartClassSchema);
+    return capitalizeFirstLetter(m[2]);
+}
+
+function extractFields(dartClassSchema) {
+
+    dartClassSchema = addCloseAccoladeifWasNotGiven(dartClassSchema);
+    dartClassSchema = dartClassSchema.replaceAll(/\s+/g, " ");
+   
+    let re = /^(class|Class)\s+[a-zA-Z_]+\s*{(.*)}\s*/;
+    let m = re.exec(dartClassSchema.trim());
+    console.log(m);
+    return m[2];
+}
+
+function addCloseAccoladeifWasNotGiven(schema) {
+    if (schema.includes("}")) {
+        return schema;
+    }
+    schema += '}';
+    return schema;
+}
+
+function parseFieldsStringToFieldObject(fieldString) {
+    let fieldsObjectsArray = [];
+    fieldString = fieldString.replaceAll(/\s+/g, " ");
+    fieldString = fieldString.split(";");
+    fieldString.pop();
+
+    fieldString.forEach(element => {
+        fieldsObjectsArray.push(convertToFieldObject(element))
+    });
+
+    return fieldsObjectsArray;
+}
+
+function convertToFieldObject(fieldStringItem) {
+    fieldStringItem = fieldStringItem.trim().split(" ");
+    return new Field(fieldStringItem[0], fieldStringItem[1]);
+}
+
+function renderDummyDataWithParameters(fieldsObjectsArray, classSchema) {
+    let className = extractClassName(classSchema);
+    let renderer = `<span class='className'>${className}</span> <span class='objectName'>${getDummyClassName()} </span>= new  <span class='className'>${className}(</span><br>`;
+    fieldsObjectsArray.forEach(fieldObject => {
+        renderer += `
+        <span class='fieldItem'>
+             <span class='fieldName'>${fieldObject.label} </span>:<span class="fieldValue">${generateRandom(fieldObject.type)}</span>,
+        </span>
+        <br>`
+    });
+    renderer += `<span class='lastParentheses'>)</span><span class='lastSemiColon'>;</span>`;
+
+    return renderer;
+}
+
+function getDummyClassName() {
+    let name = generateRandom('String').toLowerCase();
+    return name.replaceAll(/"/g, '').toLowerCase();
+}
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+
+
